784c4f82c9dbcf5c261ed36b546d743e9a126760
François Risoud <francois.risoud@gmail.com>
2022-06-03 20:19:46 +0200

    feat(style): build fifteen theme separately

---
+++ scripts/build-theme.mjs
@@ -0,0 +1,16 @@
+#!/usr/bin/env zx
+import * as fs from 'fs/promises';
+import * as path from 'path';
+import stylus from 'stylus';
+
+const themeFile = path.resolve(__dirname, '../styles/theme.styl');
+const outFile = path.resolve(__dirname, '../dist/theme.css');
+
+const themeSource = await fs.readFile(themeFile, 'utf8');
+
+stylus(themeSource)
+  .set('paths', [path.resolve(__dirname, '../styles')])
+  .render(function (err, css) {
+    if (err) throw err;
+    fs.writeFile(outFile, css, 'utf8');
+  });

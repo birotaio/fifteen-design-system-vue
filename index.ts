0cea59f8432f23402cf81f60dbceae68560281dd
François Risoud <francois.risoud@gmail.com>
2022-06-03 19:45:36 +0200

    feat(core): add lib index file

---
+++ index.ts
@@ -0,0 +1,74 @@
+/// <reference path="types/global.d.ts" />
+/**
+ * Components
+ */
+export { default as FCard } from '@/components/FCard.vue';
+export { default as FAvatar } from '@/components/FAvatar.vue';
+export { default as FBackgroundImage } from '@/components/FBackgroundImage.vue';
+export { default as FBreadcrumbs } from '@/components/FBreadcrumbs.vue';
+export { default as FButton } from '@/components/FButton.vue';
+export { default as FCheckbox } from '@/components/FCheckbox.vue';
+export { default as FDivider } from '@/components/FDivider.vue';
+export { default as FExpandable } from '@/components/FExpandable.vue';
+export { default as FFieldHint } from '@/components/FFieldHint.vue';
+export { default as FForm } from '@/components/FForm.vue';
+export { default as FGrid } from '@/components/FGrid.vue';
+export { default as FGridItem } from '@/components/FGridItem.vue';
+export { default as FIcon } from '@/components/FIcon.vue';
+export { default as FImage } from '@/components/FImage.vue';
+export { default as FInput } from '@/components/FInput.vue';
+export { default as FLink } from '@/components/FLink.vue';
+export { default as FLogo } from '@/components/FLogo.vue';
+export { default as FLogoAnimatable } from '@/components/FLogoAnimatable.vue';
+export { default as FProgressBar } from '@/components/FProgressBar.vue';
+export { default as FSelect } from '@/components/FSelect.vue';
+export { default as FSkeleton } from '@/components/FSkeleton.vue';
+export { default as FSvgImage } from '@/components/FSvgImage.vue';
+export { default as FTextarea } from '@/components/FTextarea.vue';
+/**
+ * Components props interfaces
+ */
+export type { FCardProps } from '@/components/FCard.vue';
+export type { FAvatarProps } from '@/components/FAvatar.vue';
+export type { FBackgroundImageProps } from '@/components/FBackgroundImage.vue';
+export type { FBreadcrumbsProps } from '@/components/FBreadcrumbs.vue';
+export type { FButtonProps } from '@/components/FButton.vue';
+export type { FCheckboxProps } from '@/components/FCheckbox.vue';
+export type { FDividerProps } from '@/components/FDivider.vue';
+export type { FExpandableProps } from '@/components/FExpandable.vue';
+export type { FFieldHintProps } from '@/components/FFieldHint.vue';
+export type { FFormProps } from '@/components/FForm.vue';
+export type { FGridProps } from '@/components/FGrid.vue';
+export type { FGridItemProps } from '@/components/FGridItem.vue';
+export type { FIconProps } from '@/components/FIcon.vue';
+export type { FImageProps } from '@/components/FImage.vue';
+export type { FInputProps } from '@/components/FInput.vue';
+export type { FLinkProps } from '@/components/FLink.vue';
+export type { FLogoProps } from '@/components/FLogo.vue';
+export type { FLogoAnimatableProps } from '@/components/FLogoAnimatable.vue';
+export type { FProgressBarProps } from '@/components/FProgressBar.vue';
+export type { FSelectProps } from '@/components/FSelect.vue';
+export type { FSkeletonProps } from '@/components/FSkeleton.vue';
+export type { FSvgImageProps } from '@/components/FSvgImage.vue';
+export type { FTextareaProps } from '@/components/FTextarea.vue';
+/**
+ * Composables
+ */
+export { useFBreakpoints } from '@/composables/useFBreakpoints';
+export { useFieldWithValidation } from '@/composables/useFieldWithValidation';
+export { useIcon } from '@/composables/useIcon';
+export { useInputEventBindings } from '@/composables/useInputEventBindings';
+export { useLazyImage } from '@/composables/useLazyImage';
+export { useVModelProxy } from '@/composables/useVModelProxy';
+/**
+ * Utils
+ */
+export { genSize } from '@/utils/genSize';
+export { getCssColor } from '@/utils/getCssColor';
+/**
+ * Config constants
+ */
+export { breakpointsValues } from '@/config/breakpoints';
+export { baseColorDesignTokens, colorDesignTokens } from '@/config/colors';
+export { columns, spanValues } from '@/config/grid';
+export { typographies } from '@/config/typography';

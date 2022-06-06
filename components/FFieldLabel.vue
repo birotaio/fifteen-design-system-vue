fd3671d759f73ca2c799df8373182e471017f617
Arthur Brière <arthur.briere@outlook.fr>
2022-06-06 11:50:43 +0200

    feature(FFieldLabel): add field label component

---
+++ components/FFieldLabel.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+.FFieldLabel(:for="name")
+  label {{ label }}
+</template>
+
+<style lang="stylus">
+.FFieldLabel
+  margin-bottom rem(8)
+</style>
+
+<script setup lang="ts">
+export interface FFieldLabelProps {
+  /**
+   * Label text
+   */
+  label: string;
+  /**
+   * Label field reference
+   */
+  name: string;
+  /**
+   * Label text color
+   */
+  textColor?: Color;
+}
+
+withDefaults(defineProps<FFieldLabelProps>(), {
+  textColor: 'neutral--dark-4',
+});
+</script>

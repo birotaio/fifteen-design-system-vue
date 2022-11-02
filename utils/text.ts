ae93d9b981135d2779b783f129d1dbeb02708ca1
FRobaglia <felix.robaglia@gmail.com>
2022-11-02 15:59:12 +0100

    feat(utils): add escapeRegex util

--- utils/text.ts
+++ utils/text.ts
@@ -5,3 +5,12 @@
 export function removeDiacritics(text: string): string {
   return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 }
+
+/**
+ * Escape string for use in regular expression
+ * @param str - The input string to escape
+ * @returns The escaped string to be used in RegExp constructor
+ */
+ export function escapeRegex(str: string) {
+  return str.replace(/[. *+?^${}()|[\]\\]/g, '\\$&');
+}

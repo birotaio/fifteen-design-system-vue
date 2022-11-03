e4fb2036cfdc54a20651f5d33a1ffa09873c61e0
FRobaglia <felix.robaglia@gmail.com>
2022-11-03 15:57:57 +0100

    fix: handle ligature characters in composeSearchRegex

--- utils/text.ts
+++ utils/text.ts
@@ -1,4 +1,4 @@
-const accentMap: Record<string, string> = {
+const accentCharactersMap: Record<string, string> = {
   a: '(a|á|ă|ắ|ặ|ằ|ẳ|ẵ|ǎ|â|ấ|ậ|ầ|ẩ|ẫ|ä|ǟ|ȧ|ǡ|ạ|ȁ|à|ả|ȃ|ā|ą|ᶏ|ẚ|å|ǻ|ḁ|ⱥ|ã)',
   c: '(c|ć|č|ç|ḉ|ĉ|ɕ|ċ|ƈ|ȼ)',
   e: '(e|é|ĕ|ě|ȩ|ḝ|ê|ế|ệ|ề|ể|ễ|ḙ|ë|ė|ẹ|ȅ|è|ẻ|ȇ|ē|ḗ|ḕ|ⱸ|ę|ᶒ|ɇ|ẽ|ḛ)',
@@ -6,8 +6,13 @@ const accentMap: Record<string, string> = {
   n: '(n|ń|ň|ņ|ṋ|ȵ|ṅ|ṇ|ǹ|ɲ|ṉ|ƞ|ᵰ|ᶇ|ɳ|ñ)',
   o: '(o|ó|ŏ|ǒ|ô|ố|ộ|ồ|ổ|ỗ|ö|ȫ|ȯ|ȱ|ọ|ő|ȍ|ò|ỏ|ơ|ớ|ợ|ờ|ở|ỡ|ȏ|ō|ṓ|ṑ|ǫ|ǭ|ø|ǿ|õ|ṍ|ṏ|ȭ)',
   u: '(u|ú|ŭ|ǔ|û|ṷ|ü|ǘ|ǚ|ǜ|ǖ|ṳ|ụ|ű|ȕ|ù|ủ|ư|ứ|ự|ừ|ử|ữ|ȗ|ū|ṻ|ų|ᶙ|ů|ũ|ṹ|ṵ)',
-  ae: '(ae|æ|ǽ|ǣ)',
 };
+
+const ligatureCharactersMap: Record<string, string> = {
+  oe: '(oe|ᴔ|œ|Œ)',
+  ue: '(ue|ᵫ)',
+  ae: '(ae|æ|ǽ|ǣ|ᴁ)',
+}
 
 /**
  * Escape string for use in regular expression
@@ -24,7 +29,8 @@ export function escapeRegex(str: string) {
  * @returns A search regex to be used in RegExp constructor
  */
 export function composeSearchRegex(input: string) {
-  const accentRegex = new RegExp(Object.keys(accentMap).join('|'), 'g');
+  const accentCharactersRegex = new RegExp(Object.keys(accentCharactersMap).join('|'), 'g');
+  const ligatureCharactersRegex = new RegExp(Object.keys(ligatureCharactersMap).join('|'), 'g');
 
-  return (`(${escapeRegex(input).toLowerCase().replace(accentRegex, m => accentMap[m] ?? m)})`)
+  return (`(${escapeRegex(input).toLowerCase().replace(accentCharactersRegex, m => accentCharactersMap[m] ?? m)}|${escapeRegex(input).toLowerCase().replace(ligatureCharactersRegex, m => ligatureCharactersMap[m] ?? m)})`)
 }

a18071c3b9a2e688f4c1099d77418a9b7b402112
François Risoud <francois.risoud@gmail.com>
2022-12-14 10:40:34 +0100

    fix: add test for factor, allow Ref, fix genSize behavior in all cases

--- utils/genSize.test.ts
+++ utils/genSize.test.ts
@@ -43,3 +43,43 @@ describe('Generate size', () => {
     expect(genSize(ref('8zm'))).toBe('0.5rem');
   });
 });
+
+describe('Multiplies generated size by a factor', () => {
+  it('Returns an empty string if input is empty', () => {
+    expect(genSize('', 2)).toBe('');
+    expect(genSize(null, 2)).toBe('');
+  });
+  it('Returns a size in rem if input is a number, doubled', () => {
+    expect(genSize(12, 2)).toBe('1.5rem');
+  });
+  it('Returns a size in rem if input is a pixel number, doubled', () => {
+    expect(genSize('12px', 2)).toBe('1.5rem');
+  });
+  it('Returns a size in rem if input has no unit, doubled', () => {
+    expect(genSize('12', 2)).toBe('1.5rem');
+  });
+  it('Returns the input size if it has valid CSS unit, doubled', () => {
+    cssUnitsWithoutPixel.forEach(unit =>
+      expect(genSize('5' + unit, 2)).toBe('10' + unit)
+    );
+  });
+  it('Returns the input size if it a string', () => {
+    expect(genSize('auto', 2)).toBe('auto');
+  });
+  it('Always returns 0rem if input is zero', () => {
+    expect(genSize(0, 2)).toBe('0rem');
+  });
+  it('Returns the input size casted into a pixel number if it has an invalid CSS unit, doubled', () => {
+    expect(genSize('16zm', 2)).toBe('2rem');
+  });
+  it('All the above works with refs', () => {
+    expect(genSize(ref(''), ref(2))).toBe('');
+    expect(genSize(ref(null), ref(2))).toBe('');
+    expect(genSize(ref(12), ref(2))).toBe('1.5rem');
+    expect(genSize(ref('12'), ref(2))).toBe('1.5rem');
+    cssUnitsWithoutPixel.forEach(unit =>
+      expect(genSize(ref('5' + unit), ref(2))).toBe('10' + unit)
+    );
+    expect(genSize(ref('8zm'), ref(2))).toBe('1rem');
+  });
+});
--- utils/genSize.ts
+++ utils/genSize.ts
@@ -21,7 +21,7 @@ export const cssUnits = [
 
 type SizeRaw = string | number | null | undefined;
 
-const unitRegexp = new RegExp('^(-?[0-9.]+)([a-z]+)?$', 'i');
+const unitRegexp = new RegExp('^(-?[0-9.]+)([a-z%]+)?$', 'i');
 
 /**
  * Generate proper CSS size value from a number or string, where px are converted into rem.
@@ -29,20 +29,29 @@ const unitRegexp = new RegExp('^(-?[0-9.]+)([a-z]+)?$', 'i');
  * @param factor - Optional factor to multiply with the input size, defaults to 1.
  * @returns Proper CSS size value
  */
-export function genSize(size: Ref<SizeRaw> | SizeRaw, factor = 1): string {
+export function genSize(
+  size: Ref<SizeRaw> | SizeRaw,
+  factor?: Ref<number | null | undefined> | number | null
+): string {
   const sizeValue = unref(size);
+  const factorValue = unref(factor) ?? 1;
   if (!sizeValue && sizeValue !== 0) {
     return ''; // fallback to empty string so that we handle default in CSS
   }
 
   if (typeof sizeValue === 'number') {
-    return (sizeValue * factor) / 16 + 'rem';
+    return (sizeValue * factorValue) / 16 + 'rem';
   }
 
   const cssUnitMatch = unitRegexp.exec(sizeValue);
   const [_, value, unit] = cssUnitMatch || Array(3);
-  if (value && !isNaN(+value) && (!cssUnits.includes(unit) || unit === 'px')) {
-    return (parseFloat(value) * factor) / 16 + 'rem';
+  if (value && !isNaN(+value)) {
+    // If no unit or a px unit, cast the result into `rem`
+    if (!cssUnits.includes(unit) || unit === 'px') {
+      return (parseFloat(value) * factorValue) / 16 + 'rem';
+    }
+    // Otherwize multiply the value with the factor and keep the input unit
+    return parseFloat(value) * factorValue + unit;
   }
   return sizeValue;
 }

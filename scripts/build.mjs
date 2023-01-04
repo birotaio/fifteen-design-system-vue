15a0e6a0660fab4f5626a1800c250f7284acd181
Arthur Brière <arthur.briere@outlook.fr>
2023-01-04 16:48:00 +0100

    ci: merge CSS files from index and router, then cleanup

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -11,3 +11,15 @@ await Promise.all([
   $`yarn build:theme`,
   $`yarn build:router`,
 ]);
+
+const OUT_DIR = path.join(__dirname, '../dist');
+const STYLE_FILE = path.join(OUT_DIR, 'style.css');
+const ROUTER_STYLE_FILE = path.join(OUT_DIR, 'router-style.css');
+
+const files = await Promise.all([
+  fs.readFile(STYLE_FILE, 'utf8'),
+  fs.readFile(ROUTER_STYLE_FILE, 'utf8'),
+]);
+
+await fs.writeFile(STYLE_FILE, files.join(''));
+await fs.unlink(ROUTER_STYLE_FILE);

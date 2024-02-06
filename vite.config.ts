657deccadd16ae3b6e0f58ba6c9fa91c6c7067c9
François Risoud <francois.risoud@gmail.com>
2024-02-06 20:56:03 +0100

    chore(build): add build flags (remove warnings on Vue 3.4+)

--- vite.config.ts
+++ vite.config.ts
@@ -48,6 +48,11 @@ export default defineConfig({
       },
     },
   },
+  define: {
+    __VUE_OPTIONS_API__: false,
+    __VUE_PROD_DEVTOOLS__: false,
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+  },
   build: {
     emptyOutDir: false,
     target: 'esnext',

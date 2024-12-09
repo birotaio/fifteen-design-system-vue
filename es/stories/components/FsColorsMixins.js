9e031dfec31aa26d3907703ec224364464375c14
qnp <francois.risoud@gmail.com>
2024-12-09 13:01:26 +0000

    Deploying to gh-pages from @ birotaio/fifteen-design-system-vue@328e31c616f0d1da8c228b18ef97a6786e88980f 🚀

--- es/stories/components/FsColorsMixins.js
+++ es/stories/components/FsColorsMixins.js
@@ -1,75 +1,75 @@
-import { defineComponent as p, openBlock as s, createElementBlock as r, createElementVNode as n, Fragment as e, renderList as d, toDisplayString as o, normalizeClass as a, createTextVNode as u, mergeProps as f } from "vue";
-const v = { class: "FsColorsMixins" }, _ = { class: "FsColorsMixins__container" }, g = { class: "FsColorsMixins__container" }, $ = /* @__PURE__ */ p({
+import { defineComponent as v, openBlock as t, createElementBlock as r, createElementVNode as s, Fragment as e, renderList as _, toDisplayString as o, normalizeClass as x, createTextVNode as d, mergeProps as a } from "vue";
+const F = { class: "FsColorsMixins" }, M = { class: "FsColorsMixins__container" }, f = { class: "FsColorsMixins__caption" }, m = { class: "FsColorsMixins__caption" }, p = { class: "FsColorsMixins__container" }, C = { class: "FsColorsMixins__caption" }, g = { class: "FsColorsMixins__caption" }, y = /* @__PURE__ */ v({
   __name: "FsColorsMixins",
-  setup(F) {
-    const x = ["primary", "danger", "success--dark-1", "secondary--light-1"], m = ["hover", "focus"];
-    return (b, l) => (s(), r("div", v, [
-      l[6] || (l[6] = n("caption", null, "Use the following mixins", -1)),
-      l[7] || (l[7] = n("hr", null, null, -1)),
-      l[8] || (l[8] = n("span", null, "no transition", -1)),
-      n("div", _, [
-        (s(), r(e, null, d(x, (i) => n("div", {
+  setup(b) {
+    const u = ["primary", "danger", "success--dark-1", "secondary--light-1"], c = ["hover", "focus"];
+    return ($, n) => (t(), r("div", F, [
+      n[6] || (n[6] = s("div", { class: "FsColorsMixins__caption" }, "Use the following mixins", -1)),
+      n[7] || (n[7] = s("hr", null, null, -1)),
+      n[8] || (n[8] = s("span", null, "no transition", -1)),
+      s("div", M, [
+        (t(), r(e, null, _(u, (i) => s("div", {
           key: i,
           class: "FsColorsMixins__item"
         }, [
-          n("caption", null, [
-            n("code", null, o(`bg-color('${i}')`), 1)
+          s("div", f, [
+            s("code", null, o(`bg-color('${i}')`), 1)
           ]),
-          n("div", {
-            class: a(`FsColorsMixins__bg-color--${i}`)
+          s("div", {
+            class: x(`FsColorsMixins__bg-color--${i}`)
           }, null, 2),
-          l[0] || (l[0] = n("div", null, null, -1)),
-          n("caption", null, [
-            n("code", null, o(`text-color('${i}')`), 1)
+          n[0] || (n[0] = s("div", null, null, -1)),
+          s("div", m, [
+            s("code", null, o(`text-color('${i}')`), 1)
           ]),
-          n("div", {
-            class: a(`FsColorsMixins__text-color--${i}`)
+          s("div", {
+            class: x(`FsColorsMixins__text-color--${i}`)
           }, o(i), 3)
         ])), 64))
       ]),
-      (s(), r(e, null, d(m, (i) => (s(), r(e, { key: i }, [
-        n("span", null, "with transition, on " + o(i), 1),
-        n("div", g, [
-          (s(), r(e, null, d(x, (t) => n("div", {
-            key: t,
+      (t(), r(e, null, _(c, (i) => (t(), r(e, { key: i }, [
+        s("span", null, "with transition, on " + o(i), 1),
+        s("div", p, [
+          (t(), r(e, null, _(u, (l) => s("div", {
+            key: l,
             class: "FsColorsMixins__item"
           }, [
-            l[3] || (l[3] = n("caption", null, [
-              n("code", null, o("bg-color-transition()"))
+            n[3] || (n[3] = s("div", { class: "FsColorsMixins__caption" }, [
+              s("code", null, o("bg-color-transition()"))
             ], -1)),
-            n("caption", null, [
-              n("code", null, [
-                u(":" + o(i) + " ", 1),
-                l[1] || (l[1] = n("br", null, null, -1)),
-                u(" " + o(` bg-color('${t}')`), 1)
+            s("div", C, [
+              s("code", null, [
+                d(":" + o(i) + " ", 1),
+                n[1] || (n[1] = s("br", null, null, -1)),
+                d(" " + o(` bg-color('${l}')`), 1)
               ])
             ]),
-            n("div", f({
-              class: `FsColorsMixins__bg-color--${t}--${i}`,
+            s("div", a({
+              class: `FsColorsMixins__bg-color--${l}--${i}`,
               ref_for: !0
             }, i === "focus" ? { tabindex: 1 } : {}), null, 16),
-            l[4] || (l[4] = n("div", null, null, -1)),
-            l[5] || (l[5] = n("caption", null, [
-              n("code", null, o("text-color-transition()"))
+            n[4] || (n[4] = s("div", null, null, -1)),
+            n[5] || (n[5] = s("div", { class: "FsColorsMixins__caption" }, [
+              s("code", null, o("text-color-transition()"))
             ], -1)),
-            n("caption", null, [
-              n("code", null, [
-                u(":" + o(i) + " ", 1),
-                l[2] || (l[2] = n("br", null, null, -1)),
-                u(" " + o(` text-color('${t}')`), 1)
+            s("div", g, [
+              s("code", null, [
+                d(":" + o(i) + " ", 1),
+                n[2] || (n[2] = s("br", null, null, -1)),
+                d(" " + o(` text-color('${l}')`), 1)
               ])
             ]),
-            n("div", f({
-              class: `FsColorsMixins__text-color--${t}--${i}`,
+            s("div", a({
+              class: `FsColorsMixins__text-color--${l}--${i}`,
               ref_for: !0
-            }, i === "focus" ? { tabindex: 0 } : {}), o(["before", "after"].includes(i) ? "" : t), 17)
+            }, i === "focus" ? { tabindex: 0 } : {}), o(["before", "after"].includes(i) ? "" : l), 17)
           ])), 64))
         ])
       ], 64))), 64)),
-      l[9] || (l[9] = n("span", null, "etc...", -1))
+      n[9] || (n[9] = s("span", null, "etc...", -1))
     ]));
   }
 });
 export {
-  $ as default
+  y as default
 };
--- es/stories/components/FsFontsElements2.js
+++ es/stories/components/FsFontsElements2.js
@@ -1,22 +1,22 @@
-import { openBlock as n, createElementBlock as s, createElementVNode as t, Fragment as i, renderList as a, createBlock as d, resolveDynamicComponent as m, withCtx as c, createTextVNode as r, toDisplayString as o, createStaticVNode as F } from "vue";
-const p = { class: "FsFontsElements" };
+import { openBlock as n, createElementBlock as s, createElementVNode as t, Fragment as i, renderList as a, createBlock as c, resolveDynamicComponent as d, withCtx as m, createTextVNode as r, toDisplayString as o, createStaticVNode as p } from "vue";
+const F = { class: "FsFontsElements" };
 function _(v, e) {
-  return n(), s("div", p, [
+  return n(), s("div", F, [
     e[0] || (e[0] = t("caption", null, "Use the following html elements.", -1)),
     e[1] || (e[1] = t("hr", null, null, -1)),
     (n(), s(i, null, a(6, (l) => t("div", {
       key: l,
       class: "FsFontsElements__element"
     }, [
-      (n(), d(m(`h${l}`), null, {
-        default: c(() => [
+      (n(), c(d(`h${l}`), null, {
+        default: m(() => [
           r("Heading level " + o(l), 1)
         ]),
         _: 2
       }, 1024)),
       t("code", null, "h" + o(l), 1)
     ])), 64)),
-    e[2] || (e[2] = F('<div class="FsFontsElements__element"><p>Paragraph</p><code>p</code></div><div class="FsFontsElements__element"><ul><li>List item</li></ul><code>li</code></div><div class="FsFontsElements__element"><label>Label</label><code>label</code></div><div class="FsFontsElements__element"><small>Small</small><code>small</code></div><div class="FsFontsElements__element"><caption>Caption</caption><code>caption</code></div>', 5))
+    e[2] || (e[2] = p('<div class="FsFontsElements__element"><p>Paragraph</p><code>p</code></div><div class="FsFontsElements__element"><ul><li>List item</li></ul><code>li</code></div><div class="FsFontsElements__element"><label>Label</label><code>label</code></div><div class="FsFontsElements__element"><small>Small</small><code>small</code></div><div class="FsFontsElements__element"><figure><figcaption>Figure caption</figcaption></figure><code>figcaption</code></div><div class="FsFontsElements__element"><table><caption>Caption</caption></table><code>caption</code></div>', 6))
   ]);
 }
 export {
